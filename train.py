75923a5bf1b1179d480146b029724fa238b4536d
KatoTetsuro <katotetsuro@team-lab.com>
2018-10-25 14:54:44 +0900

    indent

--- train.py
+++ train.py
@@ -127,7 +127,7 @@ def main():
         train(train_loader, model, criterion, optimizer, epoch)
 
         # evaluate on validation set
-       prec1 = validate(val_loader, model, criterion)
+        prec1 = validate(val_loader, model, criterion)
 
         # remember best prec@1 and save checkpoint
         is_best = prec1 > best_prec1
